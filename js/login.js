3b81af33f9047111fcf8cbaadc888059e7b25d6e
Ness <krayton.1235@gmail.com>
2021-03-08 20:55:03 +0200

    Анимация модалок

---
+++ js/login.js
@@ -0,0 +1,59 @@
+const messageLink = document.querySelector('.button-message');
+const messagePopup = document.querySelector('.modal-form');
+const messageClose = messagePopup.querySelector('.modal-close');
+const messageLogin = messagePopup.querySelector('.modal-input');
+const messageEmail = messagePopup.querySelector('.modal-email');
+const messageForm = messagePopup.querySelector('.form-grid');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+    storage = localStorage.getItem('message');
+}catch (err) {
+    isStorageSupport = false;
+}
+
+
+messageLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    messagePopup.classList.add('modal-show');
+
+    if(storage) {
+        messageLogin.value  = storage;
+        messageEmail.focus();
+    } else {
+        messageLogin.focus();
+    }
+});
+
+messageClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    messagePopup.classList.remove('modal-show');
+    messagePopup.classList.remove('modal-error');
+});
+
+messageForm.addEventListener('submit', function (evt) {
+    if (!messageLogin.value || !messageEmail.value) {
+    
+    evt.preventDefault();
+    messagePopup.classList.remove('modal-error');
+    messagePopup.offsetWidth = messagePopup.offsetWidth;
+    messagePopup.classList.add('modal-error');
+    } else {
+        if (isStorageSupport) {        
+        localStorage.setItem('message', messageLogin.value);
+        }
+    }
+});
+
+window.addEventListener('keydown', function(evt) {
+    if(evt.keyCode === 27) {
+        if (
+            messagePopup.classList.contains('modal-show')) {
+                evt.preventDefault();
+                messagePopup.classList.remove('modal-show');
+                messagePopup.classList.remove('modal-error');
+            }
+    }
+});
